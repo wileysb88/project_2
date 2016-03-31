97a06105bbe40d6d3246b558d1c0bb991cd61111
Michael Sturrus <msturrus@gmail.com>
2016-03-31 10:23:56 -0500

    chat is basically working

--- controllers/chat.rb
+++ controllers/chat.rb
@@ -2,9 +2,8 @@ class ChatController < ApplicationController
 
 
   get '/last' do
-    # if !session[:logged_in]
-    #   session[:account_message] = "Only registered users can chat"
-    #   redirect '/users'
+      # session[:account_message] = "Only registered users can chat"
+      # redirect '/users'
     # end
 
   {
@@ -14,9 +13,21 @@ class ChatController < ApplicationController
 
   }.to_json
 
-
-
   end
+
+  post '/last' do
+    # if !session[:logged_in]
+    #   session[:account_message] = "Only registered users can chat"
+    #   redirect '/users'
+    # end
+
+    puts params
+    Chat.create chatter_id: session[:current_user_id], message: params[:message], buffer: "", latest: true
+    puts Chat.last.message
+    puts Chat.last.chatter_id
+    # redirect '/'
+  end
+
 
   get '/set' do
     Chat.create chatter_id: 1, message: "this is a test message", buffer: "This is a test buffer", latest: true
