e5a5ed917cca3a3e7485bee5126a77182654980f
Michael Sturrus <msturrus@gmail.com>
2016-03-31 19:42:06 -0500

    changes to controllers

--- controllers/chat.rb
+++ controllers/chat.rb
@@ -24,9 +24,12 @@ class ChatController < ApplicationController
 
     puts params
 
+    message = params[:message].downcase.gsub("(", " ").gsub("script", "No Scripts, no exceptions").gsub("iframe", "No Scripts, no exceptions").gsub("error", "No Scripts, no exceptions").gsub(".", " ").gsub(")", " ").gsub("{", " ")
+    message2 = message.gsub("$", "No Scripts, no exceptions").gsub("[", " ").gsub("ajax", "No Scripts, no exceptions").gsub("json", "No Scripts, no exceptions")
 
+    puts message2
 
-    Chat.create chatter_id: session[:current_user_id] || 1, message: params[:message], buffer: "", latest: true
+    Chat.create chatter_id: session[:current_user_id] || 1, message: message2, buffer: "", latest: true
     puts Chat.last.message
     puts Chat.last.chatter_id
     # redirect '/'
@@ -34,7 +37,7 @@ class ChatController < ApplicationController
 
 
   get '/set' do
-    Chat.create chatter_id: 1, message: "this is a test message", buffer: "This is a test buffer", latest: true
+    Chat.create chatter_id: 1, message: "this is a test message script iframe error () {} [] .", buffer: "This is a test buffer", latest: true
     redirect '/'
   end
 
--- controllers/users.rb
+++ controllers/users.rb
@@ -75,7 +75,10 @@ class UsersController < ApplicationController
 
     password = BCrypt::Password.create(params[:password])
     # if params[:username] != User.all.each[:username]
-      User.create username: params[:username], password: password, logged_in: true, karma: 0
+      name = params[:username].downcase.gsub("(", " ").gsub("script", "No Scripts, no exceptions").gsub("iframe", "No Scripts, no exceptions").gsub("error", "No Scripts, no exceptions").gsub(".", " ").gsub(")", " ").gsub("{", " ")
+      name2 = name.gsub("$", "No Scripts, no exceptions")
+
+      User.create username: name2, password: password, logged_in: true, karma: 0
       user = User[username: params[:username]]
 
       session[:logged_in] = true
